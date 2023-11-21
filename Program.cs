4748ceaf2df03a529d0eecae602ff626ddecaa1d
KolesnichenkoTM <151588820+KolesnichenkoTM@users.noreply.github.com>
2023-11-21 10:53:06 +1000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public static void SayGoodbye()  
+        {
+            Console.WriteLine("Goodbye World!");
+        }
+    }
+}
